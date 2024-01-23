3a9972607efa320b3ad9db22e89421ad78fbf651
Ivan Yatsko <ivanyatsko@mail.ru>
2024-01-24 00:42:55 +0300

    feat: implement fifth task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,16 +1,48 @@
 const fs = require('fs');
 const path = require('path');
 
-const pathDir = path.join(__dirname);
-const pathesFiles = [];
+const baseDir = path.join(__dirname);
+const outputPath = path.join(baseDir, 'project-dist', 'bundle.css');
+const stylesDir = path.join(baseDir, 'styles');
+const testStylesDir = path.join(baseDir, 'test-files', 'styles');
 
-fs.readdir(pathDir, 'utf-8', (err, files) => {
-  files.forEach((file) => {
-    fs.stat(path.join(pathDir, file), (err, stats) => {
-      if (stats.isDirectory() && file === 'styles') {
-        pathesFiles.push(path.join(pathDir, file));
-      }
-      console.log(pathesFiles);
+let array = [];
+
+const processStyles = (folderPath) => {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    files.forEach((file) => {
+      const filePath = path.join(folderPath, file);
+
+      fs.readFile(filePath, 'utf-8', (err, content) => {
+        if (err) {
+          console.error(`Error reading file ${filePath}: ${err}`);
+          return;
+        }
+
+        if (path.extname(file) === '.css') {
+          array.push(content);
+          fs.writeFile(outputPath, array.join('\n'), 'utf-8', (err) => {
+            if (err) {
+              console.error(`Error writing to ${outputPath}: ${err}`);
+            }
+          });
+        }
+      });
     });
   });
-});
+};
+
+const arrOfStyles = () => {
+  processStyles(stylesDir);
+};
+
+const arrOfStylesFromTestFiles = () => {
+  processStyles(testStylesDir);
+};
+
+arrOfStylesFromTestFiles();
+arrOfStyles();
